53f85b3923b83f12dcf8b035dc695eed0a3afd30
Yousef Suliman <yousef.f.suliman@gmail.com>
2021-02-25 20:01:05 +0300

    Fixed typo

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ const App = () => {
   const handleRemoveFromCart = () => null;
 
   if (isLoading) return <LinearProgress />;
-  if (error) return <div>Some went wrong ...</div> >
+  if (error) return <div>Some went wrong ...</div>
 
 
 
